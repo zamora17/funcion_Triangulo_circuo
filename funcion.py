5c1a8582e67b5d4175a8f2eaab24bfac0103527b
Wilmer Zamora <WilmerZ@MacBook-Pro-de-Wilmer.local>
2022-06-15 08:44:23 -0500

    Initial commit

---
+++ funcion.py
@@ -0,0 +1,13 @@
+#Triangulo
+def area (base,altura):
+    return(base * altura/2)
+t=area (4,5)
+print(t)
+
+#Circulo
+pi = 3.14
+def areaCirculo (radio):
+    return(radio ** 2) * pi
+c=areaCirculo(3)
+print(c)
+
